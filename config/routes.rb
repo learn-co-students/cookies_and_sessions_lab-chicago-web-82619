3b1712682d6423bac5a3f14d060907ad1a3241db
Avinash Sarguru <avinashsarguru@gmail.com>
2019-09-29 14:36:41 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index]
+
+  get "/", to: "application#home"
+
+  post "/add", to: "products#add", as: "add"
+
 end
